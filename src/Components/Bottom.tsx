c08419f0030929c789bbfac56197df73ea1edab3
kwonkangbin <kbk282655@gmail.com>
2023-03-15 23:03:15 +0900

    CREATE : Bottom.ts

---
+++ src/Components/Bottom.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Bottem(){
+    return(
+        <>
+            <div>
+                <img src="" alt="window logo" />
+                <div>Start</div>
+            </div>
+        </>
+    );
+}
