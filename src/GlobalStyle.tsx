a31d05345ff48bf4e1ef31f483b6d0637da7ccf6
kwonkangbin <kbk282655@gmail.com>
2023-03-17 08:36:03 +0900

    CEATE : GlobalStyle.tsx

---
+++ src/GlobalStyle.tsx
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+  /* reset styles */
+  *,
+  *::before,
+  *::after {
+    margin: 0;
+    padding: 0;
+    box-sizing: inherit;
+  }
+
+  html {
+    /* set base font size and line height */
+    font-size: 62.5%; /* 1rem = 10px */
+    line-height: 1.5;
+  }
+
+  body {
+    /* remove default margin and set box-sizing to border-box */
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: sans-serif;
+  }
+`;
+
+export default GlobalStyles;
