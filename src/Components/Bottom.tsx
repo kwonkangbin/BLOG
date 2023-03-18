3d7d99d769aaf66ee7f2a6ca317781c95b38fc7b
kwonkangbin <kbk282655@gmail.com>
2023-03-18 23:42:18 +0900

    CREATE : button

--- src/Components/Bottom.tsx
+++ src/Components/Bottom.tsx
@@ -5,10 +5,11 @@ import styled from "styled-components";
 export default function Bottem(){
     return(
         <Container>
-            <div>
+            <button>
+                <div></div>
                 <img src={window_logo} alt="window logo" />
-                <div>Start</div>
-            </div>
+                <span>Start</span>
+            </button>
         </Container>
     );
 }
@@ -18,11 +19,34 @@ const Container = styled.div`
     bottom: 0;
     width: 100vw;
     background-color: #dadada;
+    padding: 7px 0 7px 12px;
 
-    > div { 
+    > button { 
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        background: #7C7C7C;
+        border-bottom: 4px inset rgba(0,0,0,.5);
+        border-left: 4px inset #f0f8ff;
+        border-right: 4px inset rgba(0,0,0,.5);
+        border-top: 4px inset #f0f8ff;
+        cursor: pointer;
+
+        &:focus,
+        &:hover {
+            background: #BCBCBC;
+        }
+        
         > img {
-            width: 33px;
+            width: 35px;
             height: auto;
+        }
+
+        > span{
+            font-size: 17px;
+            font-weight: 700;
+            margin-left: 5px;
         }
     }
 `;
